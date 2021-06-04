278b086b019adc119a63fe60f5028280e73c4616
th3mike <maarcio_lacerda@hotmail.com>
2021-06-04 15:29:10 -0300

    voltando ao zero

--- pages/form-endereco-api.vue
+++ pages/form-endereco-api.vue
@@ -1,31 +1,3 @@
 <template>
-  <div id="app">
-    <h1 class="text-center">Conversor de Moedas</h1>
-
-    <div class="linha">
-      <Conversor moeda-a="USD" moeda-b="BRL"></Conversor>
-      <Conversor moeda-a="BRL" moeda-b="USD"></Conversor>
-    </div>
-
-    <div class="linha">
-      <Conversor moeda-a="CAD" moeda-b="BRL"></Conversor>
-      <Conversor moeda-a="BRL" moeda-b="CAD"></Conversor>
-    </div>
-
-    <div class="linha">
-      <Conversor moeda-a="EUR" moeda-b="BRL"></Conversor>
-      <Conversor moeda-a="BRL" moeda-b="EUR"></Conversor>
-    </div>
-  </div>
+  <h1>FORM ENDEREÇO API</h1>
 </template>
-
-<script>
-import Conversor from "../components/Conversor.vue";
-export default {
-  name: "app",
-  components: {
-    Conversor,
-  },
-};
-</script>
-
